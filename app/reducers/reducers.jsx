30dbab9a25a9ef87982b8ad6634e24512431103c
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-09-14 16:42:49 +0300

    Add redux store

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -32,19 +32,19 @@ export var todosReducer = (state = [], action) => {
           completedAt: undefined
         }
       ];
-      case 'TOGGLE_TODO':
-        return state.map((todo) => {
-          if(todo.id === action.id){
-            var nextCompleted = !todo.completed;
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if(todo.id === action.id){
+          var nextCompleted = !todo.completed;
 
-            return {
-              ...todo,
-              completed: nextCompleted,
-              completedAt: nextCompleted ? moment().unix() : undefined
-            };
-          }
-        });
-    default:
-      return state;
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment().unix() : undefined
+          };
+        }
+      });
+  default:
+    return state;
   };
 };
