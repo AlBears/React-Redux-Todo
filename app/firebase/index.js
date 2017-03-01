4bbed756fc32c93348d219b6ea3abc6780d3db8d
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-03-01 11:59:07 +0300

    move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
 var config = {
-    apiKey: "AIzaSyB72K8eGquLNbXg4Y1_4WtBGwSvqkjSIjA",
-    authDomain: "reduxtodo-6ba26.firebaseapp.com",
-    databaseURL: "https://reduxtodo-6ba26.firebaseio.com",
-    storageBucket: "reduxtodo-6ba26.appspot.com",
-    messagingSenderId: "213681148991"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch (e) {
