9daf3d4230bde9bed2cd2b3016a51edf38500bab
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-03-01 11:01:35 +0300

    change config webpack for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,12 @@ module.exports = {
 			compressor: {
 				warnings: false
 			}
-		})
+		}),
+		new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
 	],
 	output: {
 		path: __dirname,
