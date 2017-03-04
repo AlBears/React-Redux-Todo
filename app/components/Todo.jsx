22caaf423e7712500240cbf7f32e45ea53102db1
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-03-04 18:50:37 +0300

    use contentEditable refactor

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,35 +1,46 @@
-var React = require('react');
-var {connect} = require('react-redux');
-var moment = require('moment');
-var actions = require('actions');
+import React from 'react';
+import {connect} from 'react-redux';
+import moment from 'moment';
+import * as actions from 'actions';
+import Contenteditable from 'react-contenteditable';
 
 export var Todo = React.createClass({
-  editTodo() {
-    var newText = this.refs.myRef.innerText,
+
+  handleChange() {
+    var newText = this.refs.myRef.htmlEl.innerText,
         { dispatch, id } = this.props;
     dispatch(actions.startEditTextTodo(newText, id));
-  },
+    },
+
   render(){
-    var {id, text, completed, createdAt, completedAt, dispatch} = this.props;
-    var todoClassName = completed ? 'todo todo-completed' : 'todo';
-    var renderDate = () => {
-      var message = 'Created ';
-      var timestamp = createdAt;
+    var {id, text, completed, createdAt, completedAt, dispatch} = this.props,
+        todoClassName = completed ? 'todo todo-completed' : 'todo',
+        renderDate = () => {
+            var message = 'Created ',
+                timestamp = createdAt;
 
-      if(completed) {
-        message = 'Completed ';
-        timestamp = completedAt;
-      }
+            if(completed) {
+              message = 'Completed ';
+              timestamp = completedAt;
+            }
 
-      return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
-    };
+            return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+          },
+        renderTodoText = () => {
+          return <Contenteditable
+                  tagName="p"
+                  ref = "myRef"
+                  html={text}
+                  onBlur={this.handleChange}
+                />
+            };
     return (
       <div className={todoClassName} >
         <div><input type="checkbox" checked={completed} onClick={()=> {
             dispatch(actions.startToggleTodo(id, !completed));
           }}/></div>
         <div>
-          <p contentEditable = "true" onBlur={this.editTodo} ref = "myRef">{text}</p>
+          {renderTodoText()}
           <p className="todo__subtext">{renderDate()}</p>
         </div>
       </div>
